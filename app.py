b817f0e497211894b89279deb4d598f628547557
dimka2338 <liamsmit444@gmail.com>
2024-02-04 22:33:27 +0300

    переименовал requirements.txt

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ from flask_toastr import Toastr
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = str(uuid.uuid4())
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+manager = LoginManager(app)
+
+
