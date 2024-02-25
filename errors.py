e385daab702f18c2ece638638ed6ae0765f340ae
dimka2338 <liamsmit444@gmail.com>
2024-02-25 22:56:12 +0300

    feat: созданы функции обработки ошибок 404 и 500

--- errors.py
+++ errors.py
@@ -1,16 +1,23 @@
 
 from app import app
-from flask import redirect, render_template, flash, url_for, Response
+from flask import render_template
 
 
 @app.errorhandler(404)
-def error404(status) -> Response:
+def error404(status) -> str:
     """
-    Функция обрабатывает ошибку HTTP 401, перенаправляя пользователя на страницу авторизации.
-    :param status: Int(Код ошибки)
-    :return: None
+    Функция обрабатывает ошибку HTTP 404.
+    :param status: int(Код ошибки)
+    :return: 404.html (Шаблон страницы ошибки)
     """
-    flash(
-        {"title": "Внимание!", "message": "Необходимо авторизоваться."}, category="info"
-    )
-    return redirect(url_for("input_user")), 301
+    return render_template("errors/404.html")
+
+
+@app.errorhandler(500)
+def error500(status) -> str:
+    """
+    Функция обрабатывает ошибку HTTP 500.
+    :param status: int(Код ошибки)
+    :return: 500.html (Шаблон страницы ошибки)
+    """
+    return render_template("errors/500.html")
