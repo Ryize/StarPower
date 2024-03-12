f34165eabb580b2e6eb3e1b8e0b05d16b239c71c
dimka2338 <liamsmit444@gmail.com>
2024-03-12 23:47:25 +0300

    fix: сделал преобразование даты рождения и время из строки в дату и время refactor: с помощью библиотеки zodiac_sign вычисляется знак зодиака

--- controller.py
+++ controller.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+
 from flask import render_template, Response, redirect, url_for, request, flash
 from flask_login import login_required, logout_user, login_user, current_user
+from zodiac_sign import get_zodiac_sign
+
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import app, manager, db
 from business_logic import check_new_user
-from test_logic import GetHoroscope
+# from test_logic import GetHoroscope
 
 
 @app.route('/')
@@ -86,6 +90,14 @@ def profile() -> Response | str:
         return render_template('profile.html')
     user = current_user
     forms = dict(request.form)
+    birthday_str = forms['birthday']
+    birth_time_str = forms['birth_time']
+    if birthday_str:
+        forms['birthday'] = datetime.strptime(birthday_str, '%Y-%m-%d').date()
+        forms['zodiac_sign'] = get_zodiac_sign(forms['birthday'], language='en_US')
+        print(forms['zodiac_sign'])
+    if birth_time_str:
+        forms['birth_time'] = datetime.strptime(birth_time_str, '%H:%M').time()
     for key, value in forms.items():
         if value:
             setattr(user, key, value)
@@ -98,10 +110,10 @@ def horoscope(period) -> Response | str:
     """
         Views для отображения гороскопа на день
     """
-    user_horoscope = current_user.zodiac_sign
-    get_horoscope = GetHoroscope(user_horoscope, period)
-    text = get_horoscope.get_response()
-    return render_template('chat.html', text=text)
+    # user_horoscope = current_user.zodiac_sign
+    # get_horoscope = GetHoroscope(user_horoscope, period)
+    # text = get_horoscope.get_response()
+    return render_template('chat.html')
 
 
 @app.route('/natal_chart')
